8ca3c36d8b0a1b157b8ac440644a32ee910ae16a
Caldis <532607446@qq.com>
2017-01-10 23:25:05 +0800

    Initial Commit

---
+++ Mos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mos
+//
+//  Created by Cb on 2017/1/10.
+//  Copyright © 2017年 Cb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mos
+//
+//  Created by Cb on 2017/1/10.
+//  Copyright © 2017年 Cb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
