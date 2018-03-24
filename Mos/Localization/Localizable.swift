94896ab96c0af325115b4ef9efe0b14c2d104209
Caldis <532607446@qq.com>
2018-03-24 17:54:29 +0800

    更新国际化文本 | Internationalization text update

--- Mos/Localization/Localizable.swift
+++ Mos/Localization/Localizable.swift
@@ -1,7 +1,7 @@
 //
 //  Localizable.swift
 //  Mos
-//  用于代码中的文本国际化
+//  In-code text internationalization
 //  Created by Caldis on 2018/2/25.
 //  Copyright © 2018年 Caldis. All rights reserved.
 //
@@ -10,24 +10,24 @@ import Cocoa
 
 class i18n {
     
-    // 基础
+    // Global
     static let ensure = NSLocalizedString("Ensure", comment: "")
     static let cancel = NSLocalizedString("Cancel", comment: "")
     static let version = NSLocalizedString("Version", comment: "")
     
-    // 状态栏
-    static let monitor = NSLocalizedString("Monitor", comment: "")
+    // Status Bar Icon Menu
+    static let monitor = NSLocalizedString("Scroll Monitor", comment: "")
     static let preferences = NSLocalizedString("Preferences", comment: "")
     static let quit = NSLocalizedString("Quit", comment: "")
     static let hideIcon = NSLocalizedString("Hide Icon", comment: "")
     
-    // 推送
+    // Notifaction
     static let mosIsRunningInThebackground = NSLocalizedString("Mos is running in the background", comment: "")
     static let mosStatusBarIconIsHidden = NSLocalizedString("Mos's status bar icon is hidden", comment: "")
     static let gotIt = NSLocalizedString("Got it", comment: "")
     static let showIt = NSLocalizedString("Show it", comment: "")
     
-    // 偏好设置
+    // Preferences
     static let currentVersion = NSLocalizedString("Current Version", comment: "")
     
 }
