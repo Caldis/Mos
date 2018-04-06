c9398afc5927340588e9e2e6e4c0733c330013eb
Caldis <532607446@qq.com>
2018-04-06 14:29:53 +0800

    引入 LoginServiceKit | Import LoginServiceKit

---
+++ Libs/LoginServiceKit/LoginServiceKit/LoginServiceKit.h
@@ -0,0 +1,19 @@
+//
+//  LoginServiceKit.h
+//  LoginServiceKit
+//
+//  Created by 古林俊佑 on 2016/04/05.
+//  Copyright © 2016年 Shunsuke Furubayashi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LoginServiceKit.
+FOUNDATION_EXPORT double LoginServiceKitVersionNumber;
+
+//! Project version string for LoginServiceKit.
+FOUNDATION_EXPORT const unsigned char LoginServiceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginServiceKit/PublicHeader.h>
+
+
