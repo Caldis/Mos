49b30c7180c1b8ecb50c42105e5132d6630330a6
biao.chen <mail@caldis.me>
2025-01-17 11:59:33 +0800

    chore: update interceptor clean logic

--- Mos/Utils/Interceptor.swift
+++ Mos/Utils/Interceptor.swift
@@ -10,24 +10,40 @@ import Cocoa
 
 class Interceptor {
     
-    var keeper: Timer?
-    var eventTapRef: CFMachPort?
-    var runLoopSourceRef: CFRunLoopSource?
+    private var keeper: Timer?
+    private var _eventTapRef: CFMachPort?
+    private var _runLoopSourceRef: CFRunLoopSource?
+    
+    // 可访问对象只读
+    public var eventTapRef: CFMachPort? { _eventTapRef }
+    public var runLoopSourceRef: CFRunLoopSource? { _runLoopSourceRef }
     
     public init(event mask: CGEventMask, handleBy eventHandler: @escaping CGEventTapCallBack, listenOn eventTap: CGEventTapLocation, placeAt eventPlace: CGEventTapPlacement, for behaver: CGEventTapOptions) throws {
         // 创建拦截层
         guard let tap = CGEvent.tapCreate(tap: eventTap, place: eventPlace, options: behaver, eventsOfInterest: mask, callback: eventHandler, userInfo: nil) else {
             throw InterceptorError.eventTapCreationFailed
         }
-        eventTapRef = tap
-        runLoopSourceRef = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
+        _eventTapRef = tap
+        _runLoopSourceRef = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
         // 启动
         try start()
     }
     
     deinit {
-        // 停止
+        // 先停止所有操作
         stop()
+        // 清理 event tap
+        if let tap = _eventTapRef {
+            CGEvent.tapEnable(tap: tap, enable: false)
+            _eventTapRef = nil
+        }
+        // 清理 run loop source
+        if let source = _runLoopSourceRef {
+            if CFRunLoopContainsSource(CFRunLoopGetCurrent(), source, .commonModes) {
+                CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, .commonModes)
+            }
+            _runLoopSourceRef = nil
+        }
     }
 }
 
@@ -42,11 +58,15 @@ extension Interceptor {
 // MARK: - 开关
 extension Interceptor {
     @objc public func start() throws {
-        // 启动拦截层
-        guard let tap = eventTapRef, let source = runLoopSourceRef else {
+        // 创建拦截层
+        guard let tap = _eventTapRef, let source = _runLoopSourceRef else {
             throw InterceptorError.eventTapEnableFailed
         }
-        CFRunLoopAddSource(CFRunLoopGetCurrent(), source, .commonModes)
+        // 确保 source 没有被重复添加
+        if !CFRunLoopContainsSource(CFRunLoopGetCurrent(), source, .commonModes) {
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), source, .commonModes)
+        }
+        // 启动拦截层
         CGEvent.tapEnable(tap: tap, enable: true)
         // 启动守护
         keeper = Timer.scheduledTimer(
@@ -61,10 +81,13 @@ extension Interceptor {
     public func stop() {
         // 停止守护
         keeper?.invalidate()
+        keeper = nil
         // 关闭拦截层
-        if let tap = eventTapRef, let source = runLoopSourceRef {
+        if let tap = _eventTapRef, let source = _runLoopSourceRef {
             CGEvent.tapEnable(tap: tap, enable: false)
-            CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, .commonModes)
+            if CFRunLoopContainsSource(CFRunLoopGetCurrent(), source, .commonModes) {
+                CFRunLoopRemoveSource(CFRunLoopGetCurrent(), source, .commonModes)
+            }
         }
     }
 }
@@ -72,7 +95,7 @@ extension Interceptor {
 // MARK: - 状态
 extension Interceptor {
     public func isRunning() -> Bool {
-        if let tap = eventTapRef {
+        if let tap = _eventTapRef {
             return CGEvent.tapIsEnabled(tap: tap)
         } else {
             return false
@@ -81,7 +104,8 @@ extension Interceptor {
     
     public func restart() {
         stop()
-        Timer.scheduledTimer(
+        // 保存 timer 的引用以防止提前释放
+        keeper = Timer.scheduledTimer(
             timeInterval: 0.5,
             target: self,
             selector: #selector(start),
