96b6c7af98309aed6cbbbe647d3326376bcfdf15
Harun Al Rosyid <harun.konsultan@work.bri.co.id>
2025-09-15 00:59:10 +0700

    feat: add select bahasa and config i8n text translation to bahasa

---
+++ website/app/i18n/id.ts
@@ -0,0 +1,14 @@
+export const id = {
+nav_docs: "dokumen",
+nav_github: "Github",
+hero_title: "Gulir lancar dengan Mos",
+hero_description: "Mos adalah alat canggih yang memungkinkan tetikus Anda menggulir lancar di macOS.",
+download_button: "Unduh Sekarang",
+download_releaseNotes: "Catatan Rilis",
+footer_systemRequirement: "Memerlukan macOS 10.13+",
+footer_installViaHomebrew: "Instal melalui homebrew",
+homebrew_title: "Instal melalui Homebrew",
+homebrew_description: "Jalankan perintah berikut di terminal Anda:",
+homebrew_copy: "Salin",
+homebrew_copied: "Disalin!"
+}
