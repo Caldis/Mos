a5784f50d9aee0c56bd1b85b5be731b5e4d6b925
Caldis_Chen <mail@caldis.me>
2025-09-23 00:47:38 +0800

    chore: 补充其他

--- Mos/ButtonCore/KeyCode.swift
+++ Mos/ButtonCore/KeyCode.swift
@@ -27,20 +27,19 @@ struct KeyCode {
     static let rightOption: UInt16 = 61
     static let leftControl: UInt16 = 59
     static let rightControl: UInt16 = 62
-    static let function: UInt16 = 179
+    static let function: UInt16 = 63
+    static let functionDouble: UInt16 = 179
 
-    static let modifierKeys: Set<UInt16> = [54, 55, 58, 59, 60, 61, 62, 179]
+    static let modifierKeys: Set<UInt16> = [54, 55, 58, 59, 60, 61, 62, 63, 179]
 
     // F键系列
-    static let functionKeys: Set<UInt16> = [122, 120, 99, 118, 96, 97, 98, 100, 101, 111, 103, 109, 105, 107, 113]
+    static let functionKeys: Set<UInt16> = [122, 120, 99, 118, 96, 97, 98, 100, 101, 109, 103, 111, 105, 107, 113, 106, 64, 79, 80, 90] // F1-F20
 
     // 方向系列
-    static let arrowKeys: Set<UInt16> = [
-        126, 125, 123, 124, // 上下左右
-        // 116, 121, 115, 119 // 按住 FN 的上下左右 (目前不视为上下左右避免预览时不显示这些 key
-    ]
+    static let arrowKeys: Set<UInt16> = [126, 125, 123, 124] // 上下左右
 
-    // 完整键盘映射
+    /// 完整键盘映射
+    /// https://eastmanreference.com/complete-list-of-applescript-key-codes
     static let keyMap: [UInt16: String] = [
         // 字母键
         0: "A", 1: "S", 2: "D", 3: "F", 4: "H", 5: "G", 6: "Z", 7: "X", 8: "C", 9: "V",
@@ -51,11 +50,22 @@ struct KeyCode {
         // 符号键
         24: "=", 27: "-", 30: "]", 33: "[", 39: "'", 41: ";", 42: "\\", 43: ",", 44: "/", 47: ".", 50: "`",
         // 特殊键
-        49: "⎵", 51: "⌫", 53: "⎋", 76: "↩", 36: "↩", 48: "↹", 179: "Fn",
-        // F键 (兼容 MacBook 功能键)
+        49: "⎵", 51: "⌫", 53: "⎋", 76: "↩", 36: "↩", 48: "↹", 57: "CapsLock",
+        // F区
         122: "F1", 120: "F2", 99: "F3", 118: "F4", 96: "F5", 97: "F6", 98: "F7", 100: "F8",
-        101: "F9", 109: "F10", 103: "F11", 111: "F12",  105: "F13", 107: "F14", 113: "F15",
-        // 方向键
-        126: "↑", 125: "↓", 123: "←", 124: "→", 116: "↑", 121: "↓", 115: "←", 119: "→",
+        101: "F9", 109: "F10", 103: "F11", 111: "F12", 105: "F13", 107: "F14", 113: "F15", 106: "F16", 64: "F17", 79: "F18", 80: "F19", 90: "F20",
+        // F区: 不按 FN 时触发的键 (其他录制不到) 而且录制时会自带 FN 的 flag
+        160: "F3 (MissionControl)", // 多窗口
+        177: "F4 (Spotlight)", // 搜索
+        176: "F5 (Dictating)", // 听写
+        178: "F6 (DND)", // 勿扰
+        // Direction
+        126: "↑", 125: "↓", 123: "←", 124: "→",
+        // Navigation (FN + Direction)
+        116: "PageUp", 121: "PageDown", 115: "Home", 119: "End",
+        // Modifier keys
+        63: "Fn", 59: "⌃", 58: "⌥", 55: "⌘", 56: "⇧",
+        62: "⌃ (R)", 61: "⌥ (R)", 54: "⌘ (R)", 60: "⇧ (R)", // 键盘不会直接触发这些右侧修饰键, 但是系统预留了
+        179: "Fn (179)", // 179 可以通过双击 FN 触发
     ]
 }
