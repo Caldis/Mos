244ad11fe98beb40b8089027bdd08614d0150e02
Caldis_Chen <mail@caldis.me>
2026-03-04 22:23:18 +0800

    feat: Rebuild docs static assets and pages
    Regenerate Next.js build output for the docs site: update 404, index and not-found HTML/text files; replace and rename multiple _next static chunk files (add new chunks, remove obsolete ones, and update manifests); and update website TypeScript build metadata (next-env.d.ts and tsconfig.tsbuildinfo). This is the result of a fresh docs build producing new hashed assets.

---
+++ docs/_next/static/chunks/c62ecb7f1a587007.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,46424,(e,t,r)=>{"use strict";Object.defineProperty(r,"__esModule",{value:!0}),Object.defineProperty(r,"warnOnce",{enumerable:!0,get:function(){return n}});let n=e=>{}}]);
--- docs/_next/static/chunks/ff1a16fafef87110.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,33525,(e,t,r)=>{"use strict";Object.defineProperty(r,"__esModule",{value:!0}),Object.defineProperty(r,"warnOnce",{enumerable:!0,get:function(){return n}});let n=e=>{}}]);
